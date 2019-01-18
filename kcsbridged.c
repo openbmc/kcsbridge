6816cba9f1b271aae21dcdcbe3426219b39a8e31
James Feist <james.feist@linux.intel.com>
2019-01-18 09:47:03 -0800

    Respond to all dbus messages
    sd_bus says to call process until the return code is
    no longer > 0. As we only called it once, if we got messages
    frequently enough we would fail to respond to them.
    
    Tested-by: Saw faster throughput of ipmi messages on
    d-bus
    
    Change-Id: If461d9f3ae4098e542d668763ca0a28ec4b10851
    Signed-off-by: James Feist <james.feist@linux.intel.com>

--- kcsbridged.c
+++ kcsbridged.c
@@ -297,7 +297,14 @@ static int dispatch_sd_bus(struct kcsbridged_context *context)
 	int r = 0;
 
 	if (context->fds[SD_BUS_FD].revents) {
-		r = sd_bus_process(context->bus, NULL);
+		int temp = 0;
+		// docs say to call this in a loop until no events are left
+		// to be processed
+		while ((temp = sd_bus_process(context->bus, NULL)) > 0) {
+			r = 1;
+		}
+		if (temp < 0)
+			return temp;
 		if (r > 0)
 			MSG_OUT("Processed %d dbus events\n", r);
 	}
